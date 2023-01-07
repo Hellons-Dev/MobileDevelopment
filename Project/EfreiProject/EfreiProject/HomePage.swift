809bad6facbd063430fbc9021dd5c6625d283451
Hellons <72861683+Hellons-Dev@users.noreply.github.com>
2023-01-07 09:56:11 +0000

    Add files via upload

--- Project/EfreiProject/EfreiProject/HomePage.swift
+++ Project/EfreiProject/EfreiProject/HomePage.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct HomePage: View {
+    
+    @StateObject var requ = RequestFactory()
+    
     var body: some View {
+        HStack(alignment: .top){
+            VStack(alignment: .leading) {
+                NavigationView {
+                    NavigationLink(destination: ListView(req: requ)){
+                        Text("Event Schedule")
+                    }
+                }
+            }
+        }
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Event of the day:")
+            
         }
         .padding()
+        .onAppear{
+            requ.getSchedule { (errorHandle, schedules) in}
+        }
     }
 }
 
--- Project/EfreiProject/EfreiProject/ListView.swift
+++ Project/EfreiProject/EfreiProject/ListView.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
 struct ListView: View {
-    @StateObject var requ = RequestFactory()
+    //@StateObject var requ = RequestFactory()
     //var schedule: Schedule = Schedule()
+    
+    var req : RequestFactory
+    
     var body: some View {
+        
         NavigationView{
-            List(requ.schedules, id: \.self) { schedule in
+            List(req.schedules, id: \.self) { schedule in
                 
-                    NavigationLink(destination: EventView(activity: schedule)){
-                        ListEntryView(schedule: schedule)
-                    }
+                NavigationLink(destination: EventView(activity: schedule)){
+                    ListEntryView(schedule: schedule)
                 }
+            }
             .navigationTitle("Schedule")
-            .onAppear{
-                requ.getSchedule { (errorHandle, schedules) in}
-            }
-            }
         }
     }
-
+}
 
 struct NListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView(req: RequestFactory())
     }
 }
